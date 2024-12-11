1be6576dcd84165993ae20d142bfb9bf5438e4a3
rhandley1231 <107227559+rhandley1231@users.noreply.github.com>
2024-12-10 19:32:52 -0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,14 +80,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': 'localhost',  # or the server IP if it's remote
-        'PORT': '5432',
-    }
+    
 }
 
 
